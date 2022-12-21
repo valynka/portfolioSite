617f70ded5a861726f5f07069ea06be49af3dad2
Valeria Krasnykh <valerie-kras@mail.ru>
2022-12-21 19:00:32 +0300

    some changes after revert

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,21 +5,6 @@ function AboutMe () {
     return (
         <section className="about-me" id="about">
             <div className="container-xl">
-<<<<<<< HEAD
-                <div className="row">
-                    
-                    <div className="col-md-4 col-12">
-                        <img className="avatar-img" src={avatar} alt="avatar" />
-                    </div>
-                    <div className="col-md-8 col-12 about-me-text">
-                            <h1>Привет!</h1>
-                            <p>Меня зовут <b>Валерия Красных</b>. Я являюсь сертифицированным специалистом по настройке и администрированию платформы <b>GetCourse</b>.</p>
-                            <p>Также я владею знаниями <b>HTML5, CSS3 и JavaScript</b>. Увлекаюсь фотографией и кинологией.</p>
-                            <p>Ниже вы увидите примеры работ на GetCourse и во Frontend и фотогалерею, в которой целый альбом посвящен моему корги по имени Флип.</p>
-                            <p><b>Этот сайт</b> я тоже полностью написала сама, используя <b>React</b> и стили <b>Bootstrap</b>.</p>
-                            <Education edList={educationList} />
-                    </div>
-=======
                 <div className="row align-items-center">
                     <div className="col-sm-8 col-12">
                             <h1>Привет!</h1>
@@ -31,7 +16,6 @@ function AboutMe () {
                     <div className="col-sm-4 col-12">
                         <img className="avatar-img w-100" src={avatar} alt="avatar" />
                     </div>
->>>>>>> parent of 7daf59f... GetCourse added
                 </div>
             </div>
         </section>
