f76c7821956fdcba39a6fe596f05ea231b3ba39e
Valeria Krasnykh <valerie-kras@mail.ru>
2026-04-01 18:59:20 +0300

    bug with gallery fixed

--- src/functions/photoArrayMaker.js
+++ src/functions/photoArrayMaker.js
@@ -3,9 +3,9 @@ function makePhotoColl(q, folder, alt) {
   for (let i = 1; i <= q; i += 1) {
     let num = i < 10 ? `0${i}` : i;
     const photoObj = {
-      fullImg800: `/photo/${folder}/main/${folder}_${num}_800.jpg`,
-      fullImg1920: `/photo/${folder}/main/${folder}_${num}_1920.jpg`,
-      preview800: `/photo/${folder}/preview/${folder}_${num}_800.jpg`,
+      fullImg800: `/portfolioSite/photo/${folder}/main/${folder}_${num}_800.jpg`,
+      fullImg1920: `/portfolioSite/photo/${folder}/main/${folder}_${num}_1920.jpg`,
+      preview800: `/portfolioSite/photo/${folder}/preview/${folder}_${num}_800.jpg`,
       alt: `${alt} ${num}`,
     };
     coll.push(photoObj);
